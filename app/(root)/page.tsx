6403a6299664e57bb2d4dec3fc8bd60589fe32ad
Artem Koval <dakovlen@gmail.com>
2025-04-24 13:16:30 +0200

    EWO-1/Feature: add navbar, local fonts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,6 @@ export default function Home() {
   console.log("Home page -- SERVER/CLIENT");
 
   return (
-    <h1 className="text-3xl">Welcome EWO</h1>
+    <h1 className="text-2xl">Home EWO</h1>
   );
 }
