b1c2e3fcb83c5cb5153fd0d63bdad6b00197899b
Artem Koval <dakovlen@gmail.com>
2025-05-19 11:46:47 +0200

    MAIN-1/Feature: search, data, live

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import {client} from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query;
+    const params = { search: query || null };
 
-    const posts = await client.fetch(STARTUPS_QUERY);
+    const { data: posts } = await sanityFetch({query:STARTUPS_QUERY, params});
 
     return (
         <>
@@ -34,6 +35,7 @@ export default async function Home({searchParams}: {
                     )}
                 </ul>
             </section>
+            <SanityLive/>
         </>
     );
 }
