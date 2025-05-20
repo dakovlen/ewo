f2d930be74aacbb4891e379d577b7dec50dfcb03
Artem Koval <dakovlen@gmail.com>
2025-05-20 18:00:29 +0200

    MAIN-1/Chore: TEMPO

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,4 +1,4 @@
-import {Suspense} from 'react';
+// import {Suspense} from 'react';
 import {client} from "@/sanity/lib/client";
 import {STARTUP_BY_ID_QUERY} from "@/sanity/lib/queries";
 import {notFound} from "next/navigation";
@@ -7,7 +7,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import markdownit from "markdown-it";
-import {Skeleton} from "@/components/ui/skeleton";
+// import {Skeleton} from "@/components/ui/skeleton";
 
 const md = markdownit();
 
