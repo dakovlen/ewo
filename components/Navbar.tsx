ba045d57c72d8a667722323cd04996a0e848141e
Artem Koval <dakovlen@gmail.com>
2025-05-21 01:11:47 +0200

    MAIN-1/Chore: change base color, remove autentification on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
-import { auth, signOut, signIn } from "@/auth";
+// import { auth, signOut, signIn } from "@/auth";
 
 const Navbar = async () => {
-    const session = await auth()
+    // const session = await auth()
     return (
         <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
@@ -13,34 +13,34 @@ const Navbar = async () => {
                 </Link>
 
                 <div className="flex items-center gap-5 text-black">
-                    {
-                        session && session?.user ? (
-                            <>
-                                <Link href="/startup/create">
-                                    <span>Create</span>
-                                </Link>
+                    {/*{*/}
+                    {/*    session && session?.user ? (*/}
+                    {/*        <>*/}
+                    {/*            <Link href="/startup/create">*/}
+                    {/*                <span>Create</span>*/}
+                    {/*            </Link>*/}
 
-                                <form action={ async () => {
-                                    "use server";
+                    {/*            <form action={ async () => {*/}
+                    {/*                "use server";*/}
 
-                                    await signOut({redirectTo: "/"});
-                                }}>
-                                    <button type="submit">Logout</button>
-                                </form>
-                                
-                                <Link href={`/user/${session?.id}`}>
-                                    <span>{session?.user?.name}</span>
-                                </Link>
-                            </>
-                        ) : (
-                            <form action={ async () => {
-                                "use server";
+                    {/*                await signOut({redirectTo: "/"});*/}
+                    {/*            }}>*/}
+                    {/*                <button type="submit">Logout</button>*/}
+                    {/*            </form>*/}
+                    {/*            */}
+                    {/*            <Link href={`/user/${session?.id}`}>*/}
+                    {/*                <span>{session?.user?.name}</span>*/}
+                    {/*            </Link>*/}
+                    {/*        </>*/}
+                    {/*    ) : (*/}
+                    {/*        <form action={ async () => {*/}
+                    {/*            "use server";*/}
 
-                                await signIn('github')
-                            }}>
-                                <button type="submit">Login</button>
-                            </form>
-                        )}
+                    {/*            await signIn('github')*/}
+                    {/*        }}>*/}
+                    {/*            <button type="submit">Login</button>*/}
+                    {/*        </form>*/}
+                    {/*    )}*/}
                 </div>
             </nav>
         </header>
