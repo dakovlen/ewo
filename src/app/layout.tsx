4771b60e300acb4abd5fa37b5994f1a198ae65a6
Artem Koval <dakovlen@gmail.com>
2025-08-08 18:01:32 +0200

    MAIN/Chore: fix GTM add new variant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,30 +10,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
         <Script
-          id="gtm-script"
+          src="https://www.googletagmanager.com/gtag/js?id=G-VZ5KV8LGMJ"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-59QTTF7Q');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VZ5KV8LGMJ');
             `,
           }}
         />
       </head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59QTTF7Q"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-          }}
-        />
         {children}
         <Toaster
           position="top-center"
